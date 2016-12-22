7485cce8350cdeb0b9c543e9a5921501d867da5b
jpsierens <jpsierens1988@gmail.com>
2016-12-22 09:00:41 -0600

    Shutting up webpack except for errors

--- dev-server.js
+++ dev-server.js
@@ -2,7 +2,25 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config)).listen(3000, 'localhost', function (err) {
+const shutMeTheHellUp = {
+    // It suppress error shown in console, so it has to be set to false.
+    quiet: false,
+    // It suppress everything except error, so it has to be set to false as well
+    // to see success build.
+    noInfo: false,
+    stats: {
+      // Config for minimal console.log mess.
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false
+    }
+};
+
+new WebpackDevServer(webpack(config), shutMeTheHellUp).listen(3000, 'localhost', function (err) {
     if (err) {
         console.log(err);
     }
